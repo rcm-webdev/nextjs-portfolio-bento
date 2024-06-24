d9f7077d5355f3d81972adcaa342a6ab7abe4a49
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-06-24 14:06:24 -0700

    create site-config file

---
+++ config/site-config.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  creator: "Roberto Murillo",
+  title: "Full Stack Software Engineer & Creator",
+  greetings: "Hello there,",
+  intro:
+    "I am a software engineer with a deep-rooted belief in exploring the unknown, in code and the cosmos. ",
+  instruction:
+    "Please look around my digital garden, see what I've been up to, and see what we can build together. ",
+  bio: "Outside the office, you'll find me under the hood of my car, enjoying a game of pickleball or a brisk run outdoors. ",
+  email: "bobbity.bobber@majinbobbity.com",
+} as const;
