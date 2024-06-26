7e45b382c38170717c57a4688c03c705f3dd85fc
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-06-26 15:21:19 -0700

    update variants and create a default variant

--- app/components/grid-item.tsx
+++ app/components/grid-item.tsx
@@ -7,11 +7,14 @@ const variants = cva(
   {
     variants: {
       size: {
-        "1x2": "col-span-1 row-span-2",
-        "2x1": "col-span-2 row-span-1",
-        "2x2": "col-span-2 row-span-2",
-        "2x4": "col-span-2 row-span-4",
+        "1x2": "col-span-1 row-span-2 p-8",
+        "2x1": "col-span-2 row-span-1 px-8 py-4",
+        "2x2": "col-span-2 row-span-2 p-8",
+        "2x4": "col-span-2 row-span-4 relative overflow-hidden",
       },
+    },
+    defaultVariants: {
+      size: "1x2",
     },
   }
 );
