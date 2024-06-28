7814a28a17c4831170f6a3f11c28efcab7e0d4bf
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-06-27 20:09:21 -0700

    adjust gap size between grid items

--- app/components/right-panel.tsx
+++ app/components/right-panel.tsx
@@ -10,7 +10,7 @@ const RightPanel = () => {
   return (
     <div className="flex-1 md:h-full">
       {/* Grid Container */}
-      <div className="w-full md:h-full xl:overflow-y-auto p-6 grid grid-cols-2 md:grid-cols-4 auto-rows-[76px] gap-10">
+      <div className="w-full md:h-full xl:overflow-y-auto p-6 grid grid-cols-2 md:grid-cols-4 auto-rows-[76px] gap-6 xl:gap-10">
         {/* Grid Items */}
         {siteConfig.items.map((item, index) => {
           return (
