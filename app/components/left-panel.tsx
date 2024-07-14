92be4dd78a894b5ee12f579f06c05253f5a1d6a2
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-07-13 18:03:10 -0700

    update title style and add coffee chat link and build a website link

--- app/components/left-panel.tsx
+++ app/components/left-panel.tsx
@@ -2,8 +2,11 @@ import { siteConfig } from "@/config/site-config";
 import Image from "next/image";
 import React from "react";
 import profilePic from "/public/profile.webp";
-import { SiGithub, SiLinkedin, SiTwitter } from "react-icons/si";
+
 import Footer from "./footer";
+import Button from "./grid-items/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 const LeftPanel = () => {
   return (
@@ -36,7 +39,9 @@ const LeftPanel = () => {
             {siteConfig.title}
           </div>
           {/* Full Name */}
-          <h1 className="text-5xl font-bold">{siteConfig.creator} </h1>
+          <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-neutral-900/80 via-neutral-900 to-neutral-900/80  dark:from-zinc-200/60 dark:via-zinc-200 dark:to-zinc-200/60">
+            {siteConfig.creator}{" "}
+          </h1>
           {/* Greetings */}
           <h2 className="text-2xl font-semibold">{siteConfig.greetings} </h2>
           {/* Body */}
@@ -52,7 +57,22 @@ const LeftPanel = () => {
           </div>
         </div>
         {/* Buttons */}
-        <div className="flex gap-5 w-full"></div>
+        <div className="flex items-center gap-5 w-full">
+          <Link
+            className="w-full justify-center flex items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5   text-white dark:text-zinc-900 bg-gradient-to-r from-zinc-900/75 via-zinc-800 to-zinc-900/75 hover:bg-zinc-700 dark:from-white/80 dark:via-white dark:to-white/80 dark:hover:bg-white group"
+            href="/"
+          >
+            Coffee Chat{" "}
+            <ArrowRight className="w-3 h-3 tracking-normal text-primary-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1" />
+          </Link>
+          <Link
+            className="w-full  text-neutral-900 dark:text-zinc-200  dark:bg-neutral-950 text-opacity-60 hover:text-opacity-100 dark:text-opacity-60 dark:hover:text-opacity-100"
+            href="/"
+            target="_blank"
+          >
+            Build a website.
+          </Link>
+        </div>
         {/* Footer */}
 
         <div className="hidden w-full xl:flex">
